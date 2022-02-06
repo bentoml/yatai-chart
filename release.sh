a924190269ab73606f09005b449c38fcf45617fc
Bozhao <yubz86@gmail.com>
2022-02-06 13:51:14 -0800

    add release document

--- release.sh
+++ release.sh
@@ -7,20 +7,19 @@ set -e
 
 if [ "$#" -eq 1 ]; then
     YATAI_IMAGE=$1
+    VERSION=$1
+elif [ "$#" -eq 2 ]; then
+    echo "No Yatai image specified. Using default image: yatai:latest"
+    YATAI_IMAGE=$1
+    VERSION=$2
 else
-    echo "Usage: $0 <YATAI_IMAGE>"
+    echo "Usage: $0 [YATAI_IMAGE] [VERSION]"
     exit 1
 fi
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
-# Checkout the `artifacthub-io-release` branch
-echo "Checking out the artifacthub-io-release branch..."
-git checkout artifacthub-io-release
 
-
-# Update the values.yaml file
-echo "Updating the value.yaml file with the new Yatai image..."
 if [[ "$OSTYPE" =~ ^darwin ]]; then
     # Note
     # for Mac users who get an invalid command code C error... For in-place
@@ -29,23 +28,33 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
     # 's/find/replace/' /file.txt You can skip the backup by using an empty
     # string like so: sed -i '' 's/find/replace/' /file.txt –
     echo "Mac OS detected"
+    echo "Updating the value.yaml file with the new Yatai image..."
     sed -i '' "s/^  tag:.*$/  tag: $YATAI_IMAGE/" values.yaml
+    echo "Updating appVersion and version in Chart.yaml"
+    sed -i '' "s/^version:.*$/version: $YATAI_IMAGE/" Chart.yaml
+    sed -i '' "s/^appVersion:.*$/appVersion: $YATAI_IMAGE/" Chart.yaml
 else
     echo "Linux OS detected"
     sed -i "s/^  tag:.*$/  tag: $YATAI_IMAGE/" values.yaml
+    echo "Updating appVersion and version in Chart.yaml"
+    sed -i "s/^version:.*$/version: $YATAI_IMAGE/" Chart.yaml
+    sed -i "s/^appVersion:.*$/appVersion: $YATAI_IMAGE/" Chart.yaml
 fi
 
 # generate index.yaml and .tgz file
 echo "Generating index.yaml and .tgz file..."
 make generate
 
+
+# Checkout the `artifacthub-io-release` branch
+echo "Checking out the artifacthub-io-release branch..."
+git checkout artifacthub-io-release
+
 # commit the changes
 echo "Committing the changes..."
-git add values.yaml index.yaml
-
 # Find the .tgz file name and add it to the commit
 TAR_FILE=$(find . -name "*.tgz")
-git add "$TAR_FILE"
+git add "$TAR_FILE" index.yaml
 
 # commit the changes
 git commit -m "Update Yatai image to $YATAI_IMAGE"
