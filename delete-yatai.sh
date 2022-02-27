c70882111be938eec117c940565349d04d201393
JaeYoung Jo <whwodud9@gmail.com>
2022-02-28 00:47:57 +0900

    Fix typo

--- delete-yatai.sh
+++ delete-yatai.sh
@@ -4,7 +4,7 @@
 helm uninstall yatai -n yatai-system
 
 # Remove additional yatai related namespaces
-kubectl delete namesapce yatai-components
+kubectl delete namespace yatai-components
 kubectl delete namespace yatai-operators
 kubectl delete namespace yatai-builders
 kubectl delete namespace yatai
