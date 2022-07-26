8ee5107379546e600efe74c4dd0a153c3cc9546b
yetone <yetoneful@gmail.com>
2022-07-26 04:59:25 +0000

    optimize: improve the delete-yatai.sh

--- delete-yatai.sh
+++ delete-yatai.sh
@@ -2,8 +2,11 @@
 
 CURRETN_CONTEXT=$(kubectl config current-context)
 echo -e "\033[01;31mWarning: this will permanently delete all Yatai resources, existing model deployments, and in-cluster minio, postgresql DB data. Note that external DB and blob storage will not be deleted.\033[00m"
+echo -e "\033[01;31mWarning: this also means that all resources under the \033[00m\033[01;32myatai\033[00m, \033[00m\033[01;32myatai-system\033[00m, \033[00m\033[01;32myatai-operators\033[00m, and \033[00m\033[01;32myatai-components\033[00m \033[01;31mnamespaces will be permanently deleted.\033[00m"
 echo -e "\033[01;31mCurrent kubernetes context: \033[00m\033[01;32m$CURRETN_CONTEXT\033[00m"
 read -p "Are you sure to delete Yatai in cluster '$CURRETN_CONTEXT'? [y/n] " -n 1 -r
+echo # move to a new line
+read -p "(Double check) Are you sure to delete Yatai in cluster '$CURRETN_CONTEXT'? [y/n] " -n 1 -r
 echo # move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
@@ -12,7 +15,7 @@ fi
 
 echo "Uninstalling yatai helm chart from cluster.."
 set -x
-helm uninstall yatai -n yatai-system
+helm list -n yatai-system | tail -n +2 | awk '{print $1}' | xargs -I{} helm -n yatai-system uninstall {}
 set +x
 
 echo "Deleting all crd resources and BentoDeployment.."
